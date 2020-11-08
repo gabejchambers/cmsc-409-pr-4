444d4bb28dcf89637a4666866b95306cc82813bb
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-11-08 18:37:21 -0500

    preproccess complete

--- project4.py
+++ project4.py
@@ -1,24 +1,16 @@
 import re
 
 
-f = list(filter(None, open("test_paragraphs.txt", "r").read().lower().split('\n')))
-stop_words = open("stop_words.txt", "r").read().split()
+def readandprocess():
+    pars = list(filter(None, open("test_paragraphs.txt", "r").read().lower().split('\n')))
+    sw = open("stop_words.txt", "r").read().split()
+    processed_pars = []
+    for par in pars:
+        processed_pars.append(
+            [word for word in re.sub(r'\s+', ' ', re.sub(r'[^a-zA-Z\s]', '', par)).strip().split() if word not in sw])
+    return processed_pars
 
-new_f = []
-newest_f = []
-final_f = []
-for par in f:
-    # new_f.append(re.sub(r'\s+', ' ', re.sub(r'[^a-zA-Z\s]', '', par)).strip().split())
-    # newest_f.append(re.sub(r'\s+', ' ', re.sub(r'[^a-zA-Z\s]', '', par)).strip().split())
-    # t_par = re.sub(r'\s+', ' ', re.sub(r'[^a-zA-Z\s]', '', par)).strip().split()
-    final_f.append([word for word in re.sub(r'\s+', ' ', re.sub(r'[^a-zA-Z\s]', '', par)).strip().split() if word not in stop_words])
 
-# newest_f = []
-# for par in new_f:
-#     newest_f.append(par.strip().split())
-
-# final_f = []
-# for par in newest_f:
-#     final_f.append([word for word in par if word not in stop_words])
-
-print(final_f)
+if __name__ == '__main__':
+    processed_pars = readandprocess()
+    print(processed_pars)
