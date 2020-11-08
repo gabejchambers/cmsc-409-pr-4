b52c40e208314cb799d9888a1f17f079d7e045eb
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-11-08 18:24:34 -0500

    all preprocessing steps (in commnet, sohlud be all total), done, need clean

--- project4.py
+++ project4.py
@@ -4,6 +4,8 @@ import re
 # f = open("test_paragraphs.txt", "r").read().split('\n')#.lower().split()
 # f = list(filter(None, f))
 f = list(filter(None, open("test_paragraphs.txt", "r").read().lower().split('\n')))#.split() #THIS GET ALL PARAGRAPHS INTO A LIST OF INDV STRINGS AS LOWERCASE
+stop_words = open("stop_words.txt", "r").read().split()
+print(stop_words)
 # f= open("test_paragraphs.txt", "r").read().lower()
 print(f)
 new_f = []
@@ -18,12 +20,25 @@ for par in f:
     #     par_lst.append(par.split())
     # # print(par_lst)
     # new_f.append(par_lst)
+print(new_f)
+
+#remove stop words:
+# no_sw_paragraph = [word for word in new_f if word not in stop_words]
+# result = ' '.join(no_sw_paragraph)
+# print(no_sw_paragraph)
 
 newest_f = []
 for par in new_f:
     wrds = par.strip().split()
     newest_f.append(wrds)
+print(newest_f)
+
+final_f = []
+for par in newest_f:
+    no_sw_paragraph = [word for word in par if word not in stop_words]
+    # result = ' '.join(no_sw_paragraph)
+    final_f.append(no_sw_paragraph)
+
+print(final_f)
 
 # f = re.sub(r'\W+', '', f)
-print(new_f)
-print(newest_f)
