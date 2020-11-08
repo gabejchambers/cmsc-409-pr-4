c9db4415483610d831eea310066a51bee21b8fb1
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-11-08 18:01:56 -0500

    saving progress preproccess

--- project4.py
+++ project4.py
@@ -1,1 +1,16 @@
-print("running...")
+import re
+
+# f = open("paragraphs.txt", "r").readline().lower().split()
+# f = open("test_paragraphs.txt", "r").read().split('\n')#.lower().split()
+# f = list(filter(None, f))
+f = list(filter(None, open("test_paragraphs.txt", "r").read().lower().split('\n')))#.split() #THIS GET ALL PARAGRAPHS INTO A LIST OF INDV STRINGS AS LOWERCASE
+# f= open("test_paragraphs.txt", "r").read().lower()
+print(f)
+new_f = []
+for par in f:
+    # tmp = re.sub(r'[^a-zA-Z\d\s:]', '', par)
+    new_par = re.sub(r'\s+', ' ', re.sub(r'[^a-zA-Z\s]', '', par))
+    new_f.append(new_par)
+
+# f = re.sub(r'\W+', '', f)
+print(new_f)
