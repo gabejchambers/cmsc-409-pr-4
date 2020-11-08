fd0004e140b6ae27e9e2aebe7945914b09146c8b
gabejchambers <43391919+gabejchambers@users.noreply.github.com>
2020-11-08 18:13:00 -0500

    does the preproccessing steps listed in comment of commit
    A. Tokenize paragraphs
    B. Remove punctuation and special characters
    C. Remove numbers
    D. Convert upper-case to lower-case

--- project4.py
+++ project4.py
@@ -11,6 +11,19 @@ for par in f:
     # tmp = re.sub(r'[^a-zA-Z\d\s:]', '', par)
     new_par = re.sub(r'\s+', ' ', re.sub(r'[^a-zA-Z\s]', '', par))
     new_f.append(new_par)
+    # print(new_par)
+    #par_lst = []
+    # # par_lst.append(par_lst.split())
+    # for par in new_par:
+    #     par_lst.append(par.split())
+    # # print(par_lst)
+    # new_f.append(par_lst)
+
+newest_f = []
+for par in new_f:
+    wrds = par.strip().split()
+    newest_f.append(wrds)
 
 # f = re.sub(r'\W+', '', f)
 print(new_f)
+print(newest_f)
